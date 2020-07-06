5513e7f4fa1355dcee9d18c0da44871f9e1395e6
f117-serect <15159885771@163.com>
2020-07-06 17:50:41 +0800

    Mall

---
+++ Mall/Mall-search/src/main/java/com/bean/mall/search/MallSearchApplication.java
@@ -0,0 +1,11 @@
+package com.bean.mall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class,args);
+    }
+}
