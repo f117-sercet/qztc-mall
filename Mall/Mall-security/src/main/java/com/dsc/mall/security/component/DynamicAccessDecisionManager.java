623fba77eabcb34e3e0cfbad3aec3ace50795fae
f117-serect <15159885771@163.com>
2021-01-21 22:13:32 +0800

    更新访问权限方法

---
+++ Mall/Mall-security/src/main/java/com/dsc/mall/security/component/DynamicAccessDecisionManager.java
@@ -0,0 +1,49 @@
+package com.dsc.mall.security.component;
+
+import cn.hutool.core.collection.CollUtil;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * 动态权限决策管理器，用于判断用户是否有访问权限
+ * @author 60221
+ */
+public class DynamicAccessDecisionManager implements AccessDecisionManager {
+    @Override
+    public void decide(Authentication authentication, Object o, Collection<ConfigAttribute> collection) throws AccessDeniedException, InsufficientAuthenticationException {
+
+        //当接口未被配置资源时直接执行
+        if (CollUtil.isEmpty(collection)){
+            return;
+        }
+        Iterator<ConfigAttribute> iterator =collection.iterator();
+        while(iterator.hasNext()){
+            //访问所需资源或用户拥有资源进行比较
+             ConfigAttribute configAttribute = iterator.next();
+            String needAuthority = configAttribute.getAttribute();
+            for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+                if (needAuthority.trim().equals(grantedAuthority.getAuthority())) {
+                    return;
+                }
+            }
+        }
+        throw new AccessDeniedException("别乱来，您没有访问权限");
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute configAttribute) {
+        return false;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+}
