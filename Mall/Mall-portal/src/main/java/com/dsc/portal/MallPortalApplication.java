f555c9c760b79afcf76c849336dc48ae43f5fa8f
f117-serect <15159885771@163.com>
2021-01-23 20:16:57 +0800

    mall-protal更新

---
+++ Mall/Mall-portal/src/main/java/com/dsc/portal/MallPortalApplication.java
@@ -0,0 +1,16 @@
+package com.dsc.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author 60221
+ */
+@SpringBootApplication
+public class MallPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPortalApplication.class, args);
+    }
+}
