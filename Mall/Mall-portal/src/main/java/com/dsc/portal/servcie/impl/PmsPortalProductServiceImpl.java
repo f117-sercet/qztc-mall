9bc095d5e33efc6d71a5a1d7f4038f4506fd3250
f117-serect <15159885771@163.com>
2021-02-13 15:38:33 +0800

    前台品牌方法管理更新

--- Mall/Mall-portal/src/main/java/com/dsc/portal/servcie/impl/PmsPortalProductServiceImpl.java
+++ Mall/Mall-portal/src/main/java/com/dsc/portal/servcie/impl/PmsPortalProductServiceImpl.java
@@ -1,11 +1,9 @@
 package com.dsc.portal.servcie.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.dsc.mall.mapper.*;
-import com.dsc.mall.model.PmsProduct;
-import com.dsc.mall.model.PmsProductCategory;
-import com.dsc.mall.model.PmsProductCategoryExample;
-import com.dsc.mall.model.PmsProductExample;
+import com.dsc.mall.model.*;
 import com.dsc.portal.dao.PortalProductDao;
 import com.dsc.portal.domain.PmsPortalProductDetail;
 import com.dsc.portal.domain.PmsProductCategoryNode;
@@ -104,6 +102,49 @@ public class PmsPortalProductServiceImpl implements PmsPortalProductService {
 
     @Override
     public PmsPortalProductDetail detail(Long id) {
-        return null;
+        PmsPortalProductDetail result = new PmsPortalProductDetail();
+        //获取商品信息
+        PmsProduct product = productMapper.selectByPrimaryKey(id);
+        result.setProduct(product);
+        //获取品牌信息
+        PmsBrand brand = brandMapper.selectByPrimaryKey(product.getBrandId());
+        result.setBrand(brand);
+        //获取商品属性信息
+        PmsProductAttributeExample attributeExample = new PmsProductAttributeExample();
+        attributeExample.createCriteria().andProductAttributeCategoryIdEqualTo(product.getProductAttributeCategoryId());
+        List<PmsProductAttribute> productAttributeList = productAttributeMapper.selectByExample(attributeExample);
+        result.setProductAttributeList(productAttributeList);
+        //获取商品属性信息
+        if(CollUtil.isNotEmpty(productAttributeList)){
+            List<Long> attributeIds = productAttributeList.stream().map(PmsProductAttribute::getId).collect(Collectors.toList());
+            PmsProductAttributeValueExample attributeValueExample = new PmsProductAttributeValueExample();
+            attributeValueExample.createCriteria().andProductIdEqualTo(product.getId())
+                    .andProductAttributeIdIn(attributeIds);
+            List<PmsProductAttributeValue> productAttributeValueList = productAttributeValueMapper.selectByExample(attributeValueExample);
+            result.setProductAttributeValueList(productAttributeValueList);
+        }
+        //获取商品SKU库存信息
+        PmsSkuStockExample skuExample = new PmsSkuStockExample();
+        skuExample.createCriteria().andProductIdEqualTo(product.getId());
+        List<PmsSkuStock> skuStockList = skuStockMapper.selectByExample(skuExample);
+        result.setSkuStockList(skuStockList);
+        //商品阶梯价格设置
+        if(product.getPromotionType()==3){
+            PmsProductLadderExample ladderExample = new PmsProductLadderExample();
+            ladderExample.createCriteria().andProductIdEqualTo(product.getId());
+            List<PmsProductLadder> productLadderList = productLadderMapper.selectByExample(ladderExample);
+            result.setProductLadderList(productLadderList);
+        }
+        //商品满减价格设置
+        if(product.getPromotionType()==4){
+            PmsProductFullReductionExample fullReductionExample = new PmsProductFullReductionExample();
+            fullReductionExample.createCriteria().andProductIdEqualTo(product.getId());
+            List<PmsProductFullReduction> productFullReductionList = productFullReductionMapper.selectByExample(fullReductionExample);
+            result.setProductFullReductionList(productFullReductionList);
+        }
+        //商品可用优惠券
+        result.setCouponList(portalProductDao.getAvailableCouponList(product.getId(),product.getProductCategoryId()));
+        return result;
     }
+
 }
---
+++ Mall/Mall-portal/src/main/java/com/dsc/portal/servcie/impl/PortalBrandServiceImpl.java
@@ -0,0 +1,50 @@
+package com.dsc.portal.servcie.impl;
+
+import com.dsc.mall.api.CommonPage;
+import com.dsc.mall.mapper.PmsBrandMapper;
+import com.dsc.mall.mapper.PmsProductMapper;
+import com.dsc.mall.model.PmsBrand;
+import com.dsc.mall.model.PmsProduct;
+import com.dsc.mall.model.PmsProductExample;
+import com.dsc.portal.dao.HomeDao;
+import com.dsc.portal.servcie.PortalBrandService;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 前台品牌管理Service实现类
+ * @author 60221
+ */
+@Service
+public class PortalBrandServiceImpl implements PortalBrandService {
+
+    @Autowired
+    private HomeDao homeDao;
+    @Autowired
+    private PmsBrandMapper brandMapper;
+    @Autowired
+    private PmsProductMapper productMapper;
+    @Override
+    public List<PmsBrand> recommendList(Integer pageNum, Integer pageSize) {
+        int offset = (pageNum-1)*pageSize;
+        return homeDao.getRecommendBrandList(offset,pageSize);
+    }
+
+    @Override
+    public PmsBrand detail(Long brandId) {
+        return brandMapper.selectByPrimaryKey(brandId);
+    }
+
+    @Override
+    public CommonPage<PmsProduct> productList(Long brandId, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        PmsProductExample example = new PmsProductExample();
+        example.createCriteria().andDeleteStatusEqualTo(0)
+                .andBrandIdEqualTo(brandId);
+        List<PmsProduct> productList = productMapper.selectByExample(example);
+        return CommonPage.restPage(productList);
+    }
+}
