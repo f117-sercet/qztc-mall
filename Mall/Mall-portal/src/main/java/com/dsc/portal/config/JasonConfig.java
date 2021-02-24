3dc845f001a337153b92757cc5f7642ac699d49a
f117-serect <15159885771@163.com>
2021-02-24 22:44:33 +0800

    配置信息更新

---
+++ Mall/Mall-portal/src/main/java/com/dsc/portal/config/JasonConfig.java
@@ -0,0 +1,35 @@
+package com.dsc.portal.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Jason相关配置
+ * @Author:estic
+ * @Date: 2021/2/24 22:39
+ */
+@Configuration
+public class JasonConfig {
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+
+        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+        // Include.Include.ALWAYS 默认
+        // Include.NON_DEFAULT 属性为默认值不序列化
+        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
+        // Include.NON_NULL 属性为NULL 不序列化,就是为null的字段不参加序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // 字段保留，将null值转为"
+
+        return objectMapper;
+    }
+}
---
+++ Mall/Mall-portal/src/main/java/com/dsc/portal/config/MallSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dsc.portal.config;
+
+import com.dsc.mall.security.config.SecurityConfig;
+import com.dsc.portal.servcie.UmsMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @Author:estic
+ * mall-security相关配置
+ * @Date: 2021/2/24 22:41
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MallSecurityConfig extends SecurityConfig {
+
+    @Autowired
+    private UmsMemberService memberService;
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //获取登录用户信息
+        return username -> memberService.loadUserByUsername(username);
+    }
+}
