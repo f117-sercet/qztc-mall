0261b90a2f6af15b0117e6656748cd0b2a1efbff
f117-serect <15159885771@163.com>
2021-03-13 22:40:32 +0800

    dto 更新

--- Mall/Mall-admin/src/main/java/com/dsc/mall/servcie/OmsOrderReturnReasonService.java
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/servcie/OmsOrderReturnReasonService.java
@@ -2,6 +2,8 @@ package com.dsc.mall.servcie;
 
 import com.dsc.mall.model.OmsOrderReturnReason;
 
+import java.util.List;
+
 /**
  * 退货原因管理Service
  * @Author:estic
@@ -10,8 +12,47 @@ import com.dsc.mall.model.OmsOrderReturnReason;
 public interface OmsOrderReturnReasonService {
     /**
      * 添加退货原因
-      * @param returnReason
+     * @param returnReason
      * @return
      */
-    int  create(OmsOrderReturnReason returnReason);
+    int create(OmsOrderReturnReason returnReason);
+
+    /**
+     * 修改退货原因
+     * @param id
+     * @param returnReason
+     * @return
+     */
+    int update(Long id, OmsOrderReturnReason returnReason);
+
+    /**
+     * 批量删除退货原因
+     * @param ids
+     * @return
+     */
+    int delete(List<Long> ids);
+
+    /**
+     * 分页获取退货原因
+     * @param pageSize
+     * @param pageNum
+     * @return
+     */
+    List<OmsOrderReturnReason> list(Integer pageSize, Integer pageNum);
+
+    /**
+     * 批量修改退货原因状态
+     * @param ids
+     * @param status
+     * @return
+     */
+    int updateStatus(List<Long> ids, Integer status);
+
+    /**
+     * 获取单个退货原因详情信息
+     * @param id
+     * @return
+     */
+    OmsOrderReturnReason getItem(Long id);
 }
+
