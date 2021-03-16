6788cc28c88b446a93bfd4bfdf0d9bc426ff6038
f117-serect <15159885771@163.com>
2021-03-16 20:08:26 +0800

    admin 更新

---
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/servcie/PmsProductAttributeCategoryService.java
@@ -0,0 +1,49 @@
+package com.dsc.mall.servcie;
+
+import com.dsc.mall.dto.PmsProductAttributeCategoryItem;
+import com.dsc.mall.model.PmsProduct;
+import com.dsc.mall.model.PmsProductAttributeCategory;
+
+import java.util.List;
+
+/**
+ * 商品属性分类管理Service
+ * @Author:estic
+ * @Date: 2021/3/16 20:04
+ */
+public interface PmsProductAttributeCategoryService {
+    /**
+     * 创建属性分类
+     * @param name
+     * @return
+     */
+    int create(String name);
+
+    /**
+     * 删除属性分类
+     * @param id
+     * @return
+     */
+    int delete(Long id);
+
+    /**
+     * 获取属性分类详情
+     * @param id
+     * @return
+     */
+    PmsProductAttributeCategory getItem(Long id);
+
+    /**
+     * 分页查询属性分类
+     * @param pageSize
+     * @param pageNum
+     * @return
+     */
+    List<PmsProductAttributeCategory> getList(Integer pageSize, Integer pageNum);
+
+    /**
+     * 获取包含属性的属性分类
+     * @return
+     */
+    List<PmsProductAttributeCategoryItem> getListWithAttr();
+}
