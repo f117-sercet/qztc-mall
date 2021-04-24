b0131d749c74ff1e14b4e031b8e5ed2282202be4
f117-serect <15159885771@163.com>
2021-04-24 11:40:26 +0800

    admin Controller 方法更新

---
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/controller/CmsPrefrenceAreaController.java
@@ -0,0 +1,36 @@
+package com.dsc.mall.controller;
+
+import com.dsc.mall.api.CommonResult;
+import com.dsc.mall.model.CmsPrefrenceArea;
+import com.dsc.mall.servcie.CmsPrefrenceAreaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * 商品优选管理Service
+ * @Author:estic
+ * @Date: 2021/4/24 11:38
+ */
+@Controller
+@Api(tags = "CmsPrefrenceAreaController", description = "商品优选管理")
+@RequestMapping("/prefrenceArea")
+public class CmsPrefrenceAreaController {
+
+    @Autowired
+    private CmsPrefrenceAreaService prefrenceAreaService;
+
+    @ApiOperation("获取所有商品优选")
+    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<CmsPrefrenceArea>> listAll() {
+        List<CmsPrefrenceArea> prefrenceAreaList = prefrenceAreaService.listAll();
+        return CommonResult.success(prefrenceAreaList);
+    }
+}
