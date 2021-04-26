91f41ee8bcbc6a71cd7aa0ceeff13b7f7019c08c
f117-serect <15159885771@163.com>
2021-04-26 17:29:07 +0800

    admin controller 更新

---
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/controller/OmsOrderReturnApplyController.java
@@ -0,0 +1,17 @@
+package com.dsc.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 订单退货申请管理Controller
+ * @Author:estic
+ * @Date: 2021/4/26 8:17
+ */
+@Controller
+@Api(tags = "OmsOrderReturnApplyController", description = "订单退货申请管理")
+@RequestMapping("/returnApply")
+public class OmsOrderReturnApplyController {
+
+}
