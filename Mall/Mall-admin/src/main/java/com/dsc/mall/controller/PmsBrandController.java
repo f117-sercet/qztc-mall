4b690d2c9c8467aad064e9fa7862471dc6b8192b
f117-serect <15159885771@163.com>
2021-04-27 13:33:24 +0800

    controller 更新

--- Mall/Mall-admin/src/main/java/com/dsc/mall/controller/PmsBrandController.java
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/controller/PmsBrandController.java
@@ -1,5 +1,6 @@
 package com.dsc.mall.controller;
 
+import com.dsc.mall.api.CommonPage;
 import com.dsc.mall.api.CommonResult;
 import com.dsc.mall.dto.PmsBrandParam;
 import com.dsc.mall.model.PmsBrand;
@@ -48,6 +49,7 @@ public class PmsBrandController {
         }
         return commonResult;
     }
+
     @ApiOperation(value = "更新品牌")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     @ResponseBody
@@ -55,7 +57,7 @@ public class PmsBrandController {
                                @Validated @RequestBody PmsBrandParam pmsBrandParam) {
 
         CommonResult commonResult;
-        int count = brandService.updateBrand(id,pmsBrandParam);
+        int count = brandService.updateBrand(id, pmsBrandParam);
         if (count == 1) {
             commonResult = CommonResult.success(count);
         } else {
@@ -63,4 +65,73 @@ public class PmsBrandController {
         }
         return commonResult;
     }
+
+    @ApiOperation(value = "删除品牌")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult delete(@PathVariable("id") Long id) {
+        int count = brandService.deleteBrand(id);
+        if (count == 1) {
+            return CommonResult.success(null);
+        } else {
+            return CommonResult.failed();
+        }
     }
+
+    @ApiOperation(value = "根据品牌名称分页获取品牌列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<CommonPage<PmsBrand>> getList(@RequestParam(value = "keyword", required = false) String keyword,
+                                                      @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize) {
+
+        List<PmsBrand> brandList = brandService.listBrand(keyword, pageNum, pageSize);
+        return CommonResult.success(CommonPage.restPage(brandList));
+    }
+
+    @ApiOperation(value = "根据编号查询品牌信息")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<PmsBrand> getItem(@PathVariable("id") Long id) {
+        return CommonResult.success(brandService.getBrand(id));
+    }
+
+    @ApiOperation(value = "批量删除品牌")
+    @RequestMapping(value = "/delete/batch", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult deleteBatch(@RequestParam("ids") List<Long> ids) {
+
+        int count = brandService.deleteBrand(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation(value = "批量更新显示状态")
+    @RequestMapping(value = "/update/showStatus", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updateShowStatus(@RequestParam("ids") List<Long> ids,
+                                         @RequestParam("showStatus") Integer showStatus) {
+        int count = brandService.updateShowStatus(ids, showStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation(value = "批量更新厂家制造商状态")
+    @RequestMapping(value = "/update/factoryStatus", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updateFactoryStatus(@RequestParam("ids") List<Long> ids,
+                                            @RequestParam("factoryStatus") Integer factoryStatus) {
+        int count = brandService.updateFactoryStatus(ids, factoryStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+}
+
