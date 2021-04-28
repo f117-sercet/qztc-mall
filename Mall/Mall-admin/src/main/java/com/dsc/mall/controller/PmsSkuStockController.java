faad70ca380bfe2d2c8b43ac2bcd3799cc257d38
f117-serect <15159885771@163.com>
2021-04-28 15:47:53 +0800

    controller 更新

---
+++ Mall/Mall-admin/src/main/java/com/dsc/mall/controller/PmsSkuStockController.java
@@ -0,0 +1,45 @@
+package com.dsc.mall.controller;
+
+import com.dsc.mall.api.CommonResult;
+import com.dsc.mall.model.PmsSkuStock;
+import com.dsc.mall.servcie.PmsSkuStockService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 商品SKU库存管理Controller
+ * @Author:estic
+ * @Date: 2021/4/28 15:01
+ */
+@Controller
+@Api(tags = "PmsSkuStockController", description = "sku商品库存管理")
+@RequestMapping("/sku")
+public class PmsSkuStockController {
+
+    @Autowired
+    private PmsSkuStockService skuStockService;
+
+    @ApiOperation("根据商品编号及sku编码模糊搜索sku库存")
+    @RequestMapping(value = "/{pid}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<PmsSkuStock>> getList(@PathVariable Long pid, @RequestParam(value = "keyword",required = false) String keyword) {
+        List<PmsSkuStock> skuStockList = skuStockService.getList(pid, keyword);
+        return CommonResult.success(skuStockList);
+    }
+    @ApiOperation("批量更新sku库存信息")
+    @RequestMapping(value ="/update/{pid}",method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult update(@PathVariable Long pid,@RequestBody List<PmsSkuStock> skuStockList){
+        int count = skuStockService.update(pid,skuStockList);
+        if(count>0){
+            return CommonResult.success(count);
+        }else{
+            return CommonResult.failed();
+        }
+    }
+}
--- Mall/Mall-security/src/main/java/com/dsc/mall/security/component/DynamicSecurityMetadataSource.java
+++ Mall/Mall-security/src/main/java/com/dsc/mall/security/component/DynamicSecurityMetadataSource.java
@@ -1,11 +1,15 @@
 package com.dsc.mall.security.component;
 
+import cn.hutool.core.util.URLUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
-import java.util.Collection;
-import java.util.Map;
+import javax.annotation.PostConstruct;
+import java.util.*;
 
 /**
  * 动态权限数据源，用于获取动态权限规则
@@ -17,9 +21,34 @@ public class DynamicSecurityMetadataSource implements FilterInvocationSecurityMe
     @Autowired
     private DynamicSecurityService dynamicSecurityService;
 
+    @PostConstruct
+    public void loadDataSource() {
+        configAttributeMap = dynamicSecurityService.loadDataSource();
+    }
+
+    public void clearDataSource() {
+        configAttributeMap.clear();
+        configAttributeMap = null;
+    }
+
     @Override
     public Collection<ConfigAttribute> getAttributes(Object o) throws IllegalArgumentException {
-        return null;
+        if (configAttributeMap == null) this.loadDataSource();
+        List<ConfigAttribute> configAttributes = new ArrayList<>();
+        //获取当前访问的路径
+        String url = ((FilterInvocation) o).getRequestUrl();
+        String path = URLUtil.getPath(url);
+        PathMatcher pathMatcher = new AntPathMatcher();
+        Iterator<String> iterator = configAttributeMap.keySet().iterator();
+        //获取访问该路径所需资源
+        while (iterator.hasNext()) {
+            String pattern = iterator.next();
+            if (pathMatcher.match(pattern, path)) {
+                configAttributes.add(configAttributeMap.get(pattern));
+            }
+        }
+        // 未设置操作请求权限，返回空集合
+        return configAttributes;
     }
 
     @Override
@@ -29,6 +58,6 @@ public class DynamicSecurityMetadataSource implements FilterInvocationSecurityMe
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return false;
+        return true;
     }
 }
