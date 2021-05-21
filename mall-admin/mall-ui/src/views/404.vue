bce8fb9960510159ea1fc4285c746761d65a076d
f117-serect <15159885771@163.com>
2021-05-21 16:14:24 +0800

    前端更新

--- mall-admin/mall-ui/src/views/404.vue
+++ mall-admin/mall-ui/src/views/404.vue
@@ -1,13 +1,50 @@
 <template>
-
+  <div>
+    <div class="app-container">
+      <el-col :span="12">
+        <img :src="img_404" alt="404" class="img-style">
+      </el-col>
+      <el-col :span="12">
+        <div style="margin-left: 100px;margin-top: 60px">
+          <h1 class="color-main">OOPS!</h1>
+          <h2 style="color: #606266">页面没找到！</h2>
+          <div style="color:#909399;font-size: 14px">请检查您的输入是否正确，点击以下按钮可返回主页或者发送错误报告</div>
+          <el-button style="margin-top: 20px" type="primary" round @click="handleGoMain">返回首页</el-button>
+        </div>
+      </el-col>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "404"
-    }
+  import img_404 from '@/assets/images/gif_404.gif';
+
+  export default {
+    name: 'wrongPage',
+    data() {
+      return {
+        img_404
+      }
+    },
+    methods: {
+      handleGoMain() {
+        this.$router.push({path: '/'})
+      }
+    },
+  }
 </script>
 
 <style scoped>
+  .app-container {
+    width: 80%;
+    margin: 130px auto;
+  }
 
+  .img-style {
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+  }
 </style>
+
